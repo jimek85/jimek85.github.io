7ab1ba3a3ff28d56ada4c244a760401622150382
jimek85 <jimek85@gmail.com>
2024-07-18 00:39:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Załaduj dźwięk alarmu - przykładowe dźwięki z internetu
-const alarmSounds = {
-    sound1: new Audio('https://freesound.org/s/35809/'),
-    sound2: new Audio('https://freesound.org/data/previews/74/746474_1412419-lq.mp3')
-};
-
-let alarmEnabled = true; // Flaga określająca, czy dźwięk alarmowy jest włączony
-let currentAlarm = null; // Aktualny dźwięk alarmowy
-
 // Funkcja zwracająca formatowaną bieżącą datę (dd.mm.yyyy)
 function getFormattedDate() {
     const today = new Date();
@@ -18,31 +9,46 @@ function getFormattedDate() {
 
 // Funkcja generująca dane leków na podstawie bieżącej daty
 function getMedsData() {
+    let savedData = localStorage.getItem('medsData');
+    let savedMedsData = [];
+    if (savedData) {
+        savedMedsData = JSON.parse(savedData);
+    }
+
     const currentDate = getFormattedDate();
-    return [
-        { date: currentDate, time: '8:00', med: 'Lacidofil' },
-        { date: currentDate, time: '8:00', med: 'Octeangín' },
-        { date: currentDate, time: '8:15', med: 'Glimbax' },
-        { date: currentDate, time: '8:30', med: 'Corsodyl' },
-        { date: currentDate, time: '8:45', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '8:45', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '11:00', med: 'Amotaks' },
-        { date: currentDate, time: '14:00', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '16:00', med: 'Octeangín' },
-        { date: currentDate, time: '16:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '17:30', med: 'Lacidofil' },
-        { date: currentDate, time: '18:00', med: 'Glimbax' },
-        { date: currentDate, time: '18:30', med: 'Corsodyl' },
-        { date: currentDate, time: '19:00', med: 'Amotaks' },
-        { date: currentDate, time: '19:30', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '19:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '20:30', med: 'Lacidofil' },
-        { date: currentDate, time: '21:30', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '22:30', med: 'Octeangín' },
-        { date: currentDate, time: '23:00', med: 'Corsodyl' },
-        { date: currentDate, time: '23:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '3:00', med: 'Amotaks' }
-    ];
+
+    if (savedMedsData.length > 0 && savedMedsData[0].date === currentDate) {
+        return savedMedsData;
+    } else {
+        const medsData = [
+            { date: currentDate, time: '8:00', med: 'Lacidofil' },
+            { date: currentDate, time: '8:00', med: 'Octeangín' },
+            { date: currentDate, time: '8:15', med: 'Glimbax' },
+            { date: currentDate, time: '8:30', med: 'Corsodyl' },
+            { date: currentDate, time: '8:45', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '8:45', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '11:00', med: 'Amotaks' },
+            { date: currentDate, time: '14:00', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '16:00', med: 'Octeangín' },
+            { date: currentDate, time: '16:30', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '17:30', med: 'Lacidofil' },
+            { date: currentDate, time: '18:00', med: 'Glimbax' },
+            { date: currentDate, time: '18:30', med: 'Corsodyl' },
+            { date: currentDate, time: '19:00', med: 'Amotaks' },
+            { date: currentDate, time: '19:30', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '19:30', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '20:30', med: 'Lacidofil' },
+            { date: currentDate, time: '21:30', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '22:30', med: 'Octeangín' },
+            { date: currentDate, time: '23:00', med: 'Corsodyl' },
+            { date: currentDate, time: '23:30', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '3:00', med: 'Amotaks' }
+        ];
+
+        localStorage.setItem('medsData', JSON.stringify(medsData));
+
+        return medsData;
+    }
 }
 
 let medsData = getMedsData();
@@ -54,11 +60,14 @@ function createTable() {
         row.innerHTML = `
             <td>${entry.date}</td>
             <td>${entry.time}</td>
-            <td>${entry.med}</td>
+            <td id="medName-${index}" class="${entry.taken ? 'taken' : ''}">${entry.med}</td>
             <td><input type="checkbox" id="check-${index}" onclick="markAsTaken(${index})"></td>
-            <td><button onclick="toggleAlarm()">Włącz/Wyłącz alarm</button></td>
         `;
         tableBody.appendChild(row);
+
+        if (entry.taken) {
+            document.getElementById(`check-${index}`).checked = true;
+        }
     });
 
     loadProgress();
@@ -66,11 +75,14 @@ function createTable() {
 
 function markAsTaken(index) {
     const checkBox = document.getElementById(`check-${index}`);
+    const medName = document.getElementById(`medName-${index}`);
+    
     if (checkBox.checked) {
         medsData[index].taken = true;
-        checkTimeForAlarm(); // Sprawdź czas po oznaczeniu leku
+        medName.classList.add('taken'); // Dodaj klasę 'taken' do skreślenia nazwy leku
     } else {
         medsData[index].taken = false;
+        medName.classList.remove('taken'); // Usuń klasę 'taken' jeśli checkbox nie jest zaznaczony
     }
 
     saveProgress();
@@ -88,49 +100,10 @@ function loadProgress() {
             medsData[index].taken = entry.taken;
             if (entry.taken) {
                 document.getElementById(`check-${index}`).checked = true;
+                document.getElementById(`medName-${index}`).classList.add('taken');
             }
         });
     }
 }
 
-// Funkcja do sprawdzania czasu i odtwarzania alarmu
-function checkTimeForAlarm() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-
-    // Przykładowe warunki na odtworzenie dźwięku alarmowego (zmień warunki na swoje potrzeby)
-    const alarmTimes = ['8:00', '11:00', '19:00']; // Godziny, w których mają być odtwarzane alarmy
-    if (alarmTimes.includes(`${hours}:${String(minutes).padStart(2, '0')}`)) {
-        if (alarmEnabled) {
-            playAlarm(); // Odtwórz dźwięk alarmowy, jeśli jest włączony
-        }
-    }
-}
-
-// Funkcja do odtwarzania dźwięku alarmowego
-function playAlarm() {
-    const randomSoundIndex = Math.floor(Math.random() * Object.keys(alarmSounds).length); // Losowy dźwięk alarmowy
-    const randomSoundKey = Object.keys(alarmSounds)[randomSoundIndex];
-    currentAlarm = alarmSounds[randomSoundKey];
-    currentAlarm.play();
-}
-
-// Funkcja do zatrzymywania dźwięku alarmowego
-function stopAlarm() {
-    if (currentAlarm) {
-        currentAlarm.pause();
-        currentAlarm.currentTime = 0;
-    }
-}
-
-// Funkcja do przełączania stanu alarmu (włącz/wyłącz)
-function toggleAlarm() {
-    alarmEnabled = !alarmEnabled; // Odwróć stan flagi alarmu
-    if (!alarmEnabled) {
-        stopAlarm(); // Zatrzymaj dźwięk alarmowy, jeśli był włączony
-    }
-}
-
-// Uruchomienie generowania tabeli po załadowaniu strony
 window.onload = createTable;
