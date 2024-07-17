6adef735b5836e6f388c44eb2515452d22bb6607
jimek85 <jimek85@gmail.com>
2024-07-17 23:54:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,77 +9,39 @@ function getFormattedDate() {
 
 // Funkcja generująca dane leków na podstawie bieżącej daty
 function getMedsData() {
-    const currentDate = getFormattedDate();
-    return [
-        { date: currentDate, time: '8:00', med: 'Lacidofil' },
-        { date: currentDate, time: '8:00', med: 'Octeangín' },
-        { date: currentDate, time: '8:15', med: 'Glimbax' },
-        { date: currentDate, time: '8:30', med: 'Corsodyl' },
-        { date: currentDate, time: '8:45', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '8:45', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '11:00', med: 'Amotaks' },
-        { date: currentDate, time: '14:00', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '16:00', med: 'Octeangín' },
-        { date: currentDate, time: '16:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '17:30', med: 'Lacidofil' },
-        { date: currentDate, time: '18:00', med: 'Glimbax' },
-        { date: currentDate, time: '18:30', med: 'Corsodyl' },
-        { date: currentDate, time: '19:00', med: 'Amotaks' },
-        { date: currentDate, time: '19:30', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '19:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '20:30', med: 'Lacidofil' },
-        { date: currentDate, time: '21:30', med: 'Nystatyna zawiesina' },
-        { date: currentDate, time: '22:30', med: 'Octeangín' },
-        { date: currentDate, time: '23:00', med: 'Corsodyl' },
-        { date: currentDate, time: '23:30', med: 'Nystatyna dojelitowo' },
-        { date: currentDate, time: '3:00', med: 'Amotaks' }
-    ];
-}
-
-let medsData = getMedsData();
-
-function createTable() {
-    const tableBody = document.querySelector('#medsTable tbody');
-    medsData.forEach((entry, index) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${entry.date}</td>
-            <td>${entry.time}</td>
-            <td>${entry.med}</td>
-            <td><input type="checkbox" id="check-${index}" onclick="markAsTaken(${index})"></td>
-        `;
-        tableBody.appendChild(row);
-    });
-
-    loadProgress();
-}
-
-function markAsTaken(index) {
-    const checkBox = document.getElementById(`check-${index}`);
-    if (checkBox.checked) {
-        medsData[index].taken = true;
-    } else {
-        medsData[index].taken = false;
-    }
-
-    saveProgress();
-}
-
-function saveProgress() {
-    localStorage.setItem('medsData', JSON.stringify(medsData));
-}
-
-function loadProgress() {
-    const savedData = localStorage.getItem('medsData');
+    // Pobierz zapisane dane z localStorage
+    let savedData = localStorage.getItem('medsData');
+    let savedMedsData = [];
     if (savedData) {
-        const savedMedsData = JSON.parse(savedData);
-        savedMedsData.forEach((entry, index) => {
-            medsData[index].taken = entry.taken;
-            if (entry.taken) {
-                document.getElementById(`check-${index}`).checked = true;
-            }
-        });
+        savedMedsData = JSON.parse(savedData);
     }
-}
 
-window.onload = createTable;
+    const currentDate = getFormattedDate();
+
+    // Sprawdź czy dane są zapisane na dzisiejszy dzień
+    if (savedMedsData.length > 0 && savedMedsData[0].date === currentDate) {
+        // Dane są już zapisane na dzisiejszy dzień, zwróć je
+        return savedMedsData;
+    } else {
+        // Dane nie są zapisane na dzisiejszy dzień, wygeneruj nowe dane
+        const medsData = [
+            { date: currentDate, time: '8:00', med: 'Lacidofil' },
+            { date: currentDate, time: '8:00', med: 'Octeangín' },
+            { date: currentDate, time: '8:15', med: 'Glimbax' },
+            { date: currentDate, time: '8:30', med: 'Corsodyl' },
+            { date: currentDate, time: '8:45', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '8:45', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '11:00', med: 'Amotaks' },
+            { date: currentDate, time: '14:00', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '16:00', med: 'Octeangín' },
+            { date: currentDate, time: '16:30', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '17:30', med: 'Lacidofil' },
+            { date: currentDate, time: '18:00', med: 'Glimbax' },
+            { date: currentDate, time: '18:30', med: 'Corsodyl' },
+            { date: currentDate, time: '19:00', med: 'Amotaks' },
+            { date: currentDate, time: '19:30', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '19:30', med: 'Nystatyna dojelitowo' },
+            { date: currentDate, time: '20:30', med: 'Lacidofil' },
+            { date: currentDate, time: '21:30', med: 'Nystatyna zawiesina' },
+            { date: currentDate, time: '22:30', med: 'Octeangín' },
+       
